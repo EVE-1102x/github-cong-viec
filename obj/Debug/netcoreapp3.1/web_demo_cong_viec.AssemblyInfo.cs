75c83a4ce41d0876d1d9cd10bdfb8f967356037d
Binhs <nguyentranthanhbinh123@gmail.com>
2024-04-30 16:45:30 +0700

    4/30/2024: Inserted bootstrap 5.3.3, imported 2 important font, finished pop-up for both desktop and mobile

--- obj/Debug/netcoreapp3.1/web_demo_cong_viec.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/web_demo_cong_viec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_demo_cong_viec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b487fda5796c5924a9061bd534e950a4dc1d1aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e8869305e1347ae1519df0b68c884b791740f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_demo_cong_viec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_demo_cong_viec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
