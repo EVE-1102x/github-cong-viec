c9c73b2dbd2c1c6b35389c4fb61086ef0bd0dad7
Binhs <nguyentranthanhbinh123@gmail.com>
2024-05-02 16:01:43 +0700

    5/2/2024: Finished home page top

---
+++ Pages/Cruise.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web_demo_cong_viec.Pages
+{
+    public class CruiseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
