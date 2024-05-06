c031ce66deddb252ede08c4f1a4d912ec4654b78
dienducminh <dienducminh2201@gmail.com>
2024-05-06 23:45:51 +0700

    2

--- obj/Debug/netcoreapp3.1/web_demo_cong_viec.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/web_demo_cong_viec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_demo_cong_viec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c082fb852ae8534ed225751e132c8fa818c468e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21664ae7c25e8b1cf216f60d3d3583a0bf8f30d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_demo_cong_viec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_demo_cong_viec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
